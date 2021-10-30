8e6839df7e5934eba9ad18569c169dbfff450304
Dominik Horváth <dom.horvath17@gmail.com>
2021-10-30 19:36:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ app.get('/', (request, response) => {
     response.send("<h1>Hello from node</h1>")
 })
 
-app.listen(process.env.PORT || 3000, () => console.log("App available on 127.0.0.1:3000"))
+app.listen(process.env.PORT || 3000, () => console.log("App available on http://127.0.0.1:3000"))
