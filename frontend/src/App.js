94c2c41c18303d9c2961899ec3669d2944197407
David Silady <xsilady@stuba.sk>
2022-03-04 18:53:38 +0100

    Added react router library with basic config

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import {ThemeProvider} from '@material-ui/core/styles';
 import TitleSearch from "./Components/TitleSearch";
 import {theme} from "./Style/Theme";
+import { Routes, Route, Link } from "react-router-dom";
 
 
 
@@ -10,6 +11,8 @@ function App() {
     return (
         <div className="App" style={{maxWidth: "1920px", margin: "auto"}}>
             <ThemeProvider theme={theme}>
+                <Routes>
+                </Routes>
                 <TitleSearch/>
             </ThemeProvider>
         </div>
