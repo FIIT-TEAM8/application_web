f7771be6a074f05e65cb963cdcdcc2d5facbdfc0
DavidSilady <david.silady@gmail.com>
2022-03-21 15:05:10 +0100

    Added new dependencies and a simple logging util

--- backend/routes/endpoints/user.js
+++ backend/routes/endpoints/user.js
@@ -1,9 +1,25 @@
 // '/user/*' endpoint
-
+const jwt = require('jsonwebtoken')
 const express = require('express')
 const fetch = require('node-fetch')
-const {cfg} = require("../../config");
+const {cfg} = require("../../config")
 const router = express.Router()
+
+function authenticateToken(req, res, next) {
+    // const authHeader = req.headers['authorization']
+    // const token = authHeader && authHeader.split(' ')[1]
+    const token = req.cookies.__authToken
+    if (! token) {
+        return res.sendStatus(401)
+    }
+    
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        debug(`Authenticated: ${user.name}`)
+        req.user = user
+        next()
+    })
+}
 
 router.get('/', function (req, res) {
     res.status(200).json({ok: true, data: {}, msg: "Default user route is working"})
