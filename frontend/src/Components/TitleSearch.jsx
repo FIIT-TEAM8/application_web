b8df8279b8a5b80329e9ace03d6931e2343eefea
tannya <tani.polakova@gmail.com>
2022-04-18 10:57:22 +0200

    static data replaced with api data

--- frontend/src/Components/TitleSearch.jsx
+++ frontend/src/Components/TitleSearch.jsx
@@ -7,11 +7,6 @@ import { apiCall } from "../Utils/APIConnector";
 import AdvancedSearch from "../Components/AdvancedSearch";
 import {emptyFilters, getYears} from "../Utils/AdvancedSearchUtils";
 
-// data will be replaced
-import * as regionCodeMapping from '../Utils/region_code_maping.json';
-const KEYWORDS = ['Abusive Sexual Contact', 'Antitrust', 'Arson', 'Assassination', 'Assault', 'Attempted Murder', 'Bank Burglary', 'Bankruptcy Fraud', 'Blackmail', 'Bombing Matters', 'Bomb Threat', 'Bond Default', 'Bribery', 'Burglary', 'Child Abuse', 'Child Abandonment', 'Child Abduction', 'Child Exploitation', 'Child Pornography', 'Civil Rights Violations', 'Computer Crime', 'Conspiracy', 'Conspiracy to Murder', 'Conveying False Information', 'Copyright Matters', 'Counterfeiting', 'Credit Card Fraud', 'Cruelty of Animals', 'Cyber Crimes', 'Cyberbullying', 'Dangerous Driving', 'Death Threat', 'Domestic Violence', 'Drug Distribution', 'Drug Possession', 'Drug Smuggling', 'Drug Trafficking', 'Drunk Driving', 'Embezzlement', 'Escaping Custody', 'Exportation of Drugs', 'Extortion', 'False Bail', 'Falsely Claiming Citizenship', 'False Information and Hoaxes', 'Felony', 'First Degree Murder', 'Forced Labor', 'Forgery', 'Fraud', 'Hacking Crimes', 'Harassment', 'Hate Crime Acts', 'Homicide', 'Hostage Taking', 'Identity Theft', 'Illegal Emigration', 'Illegal Possession of Firearms', 'Importation of Drugs', 'Insurance Fraud', 'Kidnapping', 'Larceny', 'Manslaughter', 'Molestation', 'Money Laundering', 'Murder', 'Narcotics Violations', 'Pirating', 'Probation Violation', 'Prostitution', 'Racketeering', 'Ransom Money', 'Rape', 'Robbery', 'Sabotage', 'Sale of Citizenship Papers', 'Sale of Stolen Vehicles', 'Second Degree Murder', 'Serial Murders', 'Sexual Abuse', 'Sexual Assault', 'Sexual Conduct with a Minor', 'Sex Trafficking', 'Shoplifting', 'Smuggling', 'Stalking', 'Tax Evasion', 'Tax Fraud', 'Terrorism', 'Theft', 'Torture', 'Transportation of Stolen Vehicles', 'Trespassing', 'Treason', 'Vandalism', 'Wire Fraud']
-
-
 export default function TitleSearch() {
 
     const navigate = useNavigate();
