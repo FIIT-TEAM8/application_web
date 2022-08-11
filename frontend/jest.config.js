b0e7fbcc71f145cdb111f6c0900dd76ec159b552
JKBGIT1 <jakubhlavacka123@gmail.com>
2022-08-11 14:35:38 +0200

    Jest testing framework setup in jest-environment-jsdom with babel config

---
+++ frontend/jest.config.js
@@ -0,0 +1,9 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
+  }
+};
