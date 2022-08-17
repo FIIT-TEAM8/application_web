43e8136abaea06a5e89b1a2e9c48f72a979ddee5
JKBGIT1 <jakubhlavacka123@gmail.com>
2022-08-17 18:31:25 +0200

    SearchResults test pagination with original react-router-dom

--- frontend/src/Components/SearchResults.test.jsx
+++ frontend/src/Components/SearchResults.test.jsx
@@ -1,6 +1,8 @@
 import { act, render, screen } from "@testing-library/react";
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import {
+  BrowserRouter, Routes, Route, Navigate
+} from "react-router-dom";
 import { apiCall } from "../Utils/APIConnector";
 import SearchResults from "./SearchResults";
 
@@ -45,6 +47,7 @@ describe("<SearchResults />", () => {
       })
     );
 
+    // await is actually required
     await act(async () => render(
       // eslint-disable-next-line dot-notation
       <BrowserRouter basename={process.env["PUBLIC_URL"]}>
@@ -59,5 +62,81 @@ describe("<SearchResults />", () => {
       expect(articlesTitles[i]).toBeInTheDocument();
     }
     expect(articlesTitles.length).toBe(totalResults);
+  });
+
+  it("should display results with pagination", async () => {
+    const totalResults = 12;
+    const totalPages = 2;
+    const maxResultsForPage = 10;
+    const activePageIndex = 1;
+
+    const mockedData = {
+      results: [],
+      total_pages: totalPages,
+      total_results: totalResults
+    };
+
+    for (let i = 0; i < maxResultsForPage; i += 1) {
+      mockedData.results.push({
+        ...articleMock,
+        title: `Test title ${i}`, // article's title figures as key in ResultItem component (required unique)
+      });
+    }
+
+    apiCall.mockResolvedValue(
+      new Promise((resolve) => {
+        process.nextTick(() => resolve({
+          ok: true,
+          data: mockedData
+        }));
+      })
+    );
+
+    // await is actually required
+    await act(async () => render(
+      // in this case it was easier to use Routes
+      // instead of mocking useSeachrParams to get the value of page query parameter,
+      // which is required in MUI Paginaton rendering
+      // eslint-disable-next-line dot-notation
+      <BrowserRouter basename={process.env["PUBLIC_URL"]}>
+        <Routes>
+          <Route path="" element={<Navigate to="/search/results?q=test&page=1" />} />
+          <Route path="/search/results?q=test&page=1" element={<SearchResults />} />
+        </Routes>
+        <SearchResults />
+      </BrowserRouter>
+    ));
+
+    expect(screen.getByText(`${totalResults} results found.`)).toBeInTheDocument();
+
+    const articlesTitles = screen.getAllByText(/Test title/);
+    for (let i = 0; i < articlesTitles.length; i += 1) {
+      expect(articlesTitles[i]).toBeInTheDocument();
+    }
+    expect(articlesTitles.length).toBe(maxResultsForPage);
+
+    const paginationContainer = screen.getByRole("navigation", { name: "pagination navigation" });
+    expect(paginationContainer).toBeInTheDocument();
+
+    const paginationButtons = screen.getAllByRole("button", { name: /page/i });
+    const prevPageButton = paginationButtons[0];
+    expect(prevPageButton).toBeInTheDocument();
+    expect(prevPageButton).toHaveAttribute("aria-label", "Go to previous page");
+
+    const nextPageButton = paginationButtons[paginationButtons.length - 1];
+    expect(nextPageButton).toBeInTheDocument();
+    expect(nextPageButton).toHaveAttribute("aria-label", "Go to next page");
+
+    const length = paginationButtons.length - 1; // ommit next page button
+    for (let i = 1; i < length; i += 1) {
+      const currPaginationButton = paginationButtons[i];
+      expect(currPaginationButton).toBeInTheDocument();
+      // index of active page has to be equal to page query parameter in Route
+      if (i === activePageIndex) {
+        expect(currPaginationButton).toHaveAttribute("aria-label", `page ${i}`);
+      } else {
+        expect(currPaginationButton).toHaveAttribute("aria-label", `Go to page ${i}`);
+      }
+    }
   });
 });
