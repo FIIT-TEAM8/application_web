99b0d7e2907f26e52882b2fc8d4d7713cd13f49b
JKBGIT1 <jakubhlavacka123@gmail.com>
2022-08-17 16:55:22 +0200

    ResultItem test for logged user

--- frontend/src/Components/ResultItem.test.jsx
+++ frontend/src/Components/ResultItem.test.jsx
@@ -36,9 +36,12 @@ describe("<ResultItem />", () => {
 
     const titleLink = screen.getByRole("link", { name: mockedArticle.title });
     expect(titleLink).toHaveAttribute("href", mockedArticle.link);
+
     const archiveLink = screen.getByRole("link", { name: "Archived Article" });
     expect(archiveLink).toHaveAttribute("href", `/archive?link=${mockedArticle.link}`);
+
     expect(screen.getByText("04 Dec 2018")).toBeInTheDocument();
+
     const sourceLink = screen.getByRole("link", { name: "express.co.uk" });
     expect(sourceLink).toHaveAttribute("href", mockedArticle.link);
 
@@ -48,16 +51,37 @@ describe("<ResultItem />", () => {
   });
 
   it("check component rendering for logged user", () => {
+    // in this case the useUser function is mocked with minimal attributes, which are required
+    // just to test rendering of button, which adds article to logged user's report
     useUser.mockImplementation(() => ({
-      user: "tester"
+      user: "tester",
+      articlesInReport: [],
     }));
 
-    // TODO: not working, mock articlesInReport???
     render(
       // eslint-disable-next-line dot-notation
       <BrowserRouter basename={process.env["PUBLIC_URL"]}>
         <ResultItem item={mockedArticle} />
       </BrowserRouter>
     );
+
+    const titleLink = screen.getByRole("link", { name: mockedArticle.title });
+    expect(titleLink).toHaveAttribute("href", mockedArticle.link);
+
+    const archiveLink = screen.getByRole("link", { name: "Archived Article" });
+    expect(archiveLink).toHaveAttribute("href", `/archive?link=${mockedArticle.link}`);
+
+    expect(screen.getByText("04 Dec 2018")).toBeInTheDocument();
+
+    const sourceLink = screen.getByRole("link", { name: "express.co.uk" });
+    expect(sourceLink).toHaveAttribute("href", mockedArticle.link);
+
+    for (let i = 0; i < mockedArticle.keywords.length; i += 1) {
+      expect(screen.getByText(mockedArticle.keywords[i])).toBeInTheDocument();
+    }
+
+    const reportButton = screen.getByRole("button");
+    expect(reportButton).toBeInTheDocument();
+    expect(reportButton).toHaveAttribute("aria-label", "Add to PDF report");
   });
 });
